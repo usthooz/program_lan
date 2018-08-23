29c1e50ffab28f87e5964d84061b3458ac802d9d
解闯 <chuang.xie@enai.im>
2018-08-23 15:24:27 +0800

    if-else内层循环

---
+++ cpp/base/code/ifs.cpp
@@ -0,0 +1,25 @@
+/*if-else内层*/
+#include <iostream>
+using namespace std;
+main()
+{
+    int a,b,c;
+    int smallest;
+    cout<<"a  b  c"<<endl;
+    cin>>a>>b>>c;
+    if (a<=b)    //外层条件语句
+    {
+        if (a<=c)    //内层条件语句
+           smallest=a;
+        else
+           smallest=c;
+    }
+    else
+    {
+       if (b<=c)    //内层条件语句
+           smallest=b;
+       else
+           smallest=c;
+    }
+    cout<<"Smallest="<<smallest<<endl;
+}
