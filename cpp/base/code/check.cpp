565108332d1dfcb9e5c48ed50969ed55cf843562
解闯 <chuang.xie@enai.im>
2018-08-23 15:20:37 +0800

    判断计算

---
+++ cpp/base/code/check.cpp
@@ -0,0 +1,20 @@
+/*除法*/
+#include <iostream>
+using namespace std;
+main()
+{
+    int a,b;
+    //输入数据
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+ 
+    //除法判断
+    if (b!=0 && a%b==0) {
+        cout<<b<<" divides "<<a<<endl;
+        cout<<"a/b="<<a/b<<endl;
+    }
+    else
+        cout<<b<<" does not divide "<<a<<endl;
+}
