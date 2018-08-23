2b01c67faecd35cbcf2e69f596d3949decea5b4b
解闯 <chuang.xie@enai.im>
2018-08-23 15:27:13 +0800

    制表符

---
+++ cpp/base/code/tab.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+	char c1='\a',TAB='\t';
+	//阵铃一声
+	cout<<c1<<endl;
+ 
+	//水平制表
+	cout<<1<<TAB<<2<<TAB<<3<<TAB<<4<<endl;
+ 
+	//使用双引号
+	cout<<"swxc\"tx\"."<<endl;
+ 
+	//使用回车换行
+	cout<<"xctx\n"<<"0707\n";
+ 
+	return 0;
+}
