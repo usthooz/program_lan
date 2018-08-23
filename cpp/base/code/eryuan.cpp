436338d9221fc1caefd362b323dbff9b629b7bdb
解闯 <chuang.xie@enai.im>
2018-08-23 15:22:05 +0800

    二元计算

---
+++ cpp/base/code/eryuan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int a,b,Max;
+    //输入数据
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+ 
+    //找出较大值
+    Max=a>b?a:b;
+    cout<<"Max="<<Max<<endl;
+	return 0;
+}
