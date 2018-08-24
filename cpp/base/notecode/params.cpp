f7a435544e183048df436692c69f8c4dffbdaf23
解闯 <chuang.xie@enai.im>
2018-08-24 20:10:36 +0800

    增加参数传递例程

---
+++ cpp/base/notecode/params.cpp
@@ -0,0 +1,41 @@
+// 对应参数传递笔记
+#include<iostream>
+using namespace std;
+
+void Fun(int a[],int b)
+{
+      for(int i=0; i < b; i++)
+      {
+            (*a)++;
+	}
+}
+
+void Fun1(int &b)
+{
+	b++;
+}
+
+int main()
+{
+	// 数组操作
+	int array[]={1,2,3,4}; 
+	// 数组的长度
+	int length = sizeof(array)/sizeof(array[0]);
+	cout << "old data:" << endl;
+	for (int i = 0;i< length;i++)
+	{
+		cout << "Index:" << i << "  " <<"Value:" << array[i] <<endl;
+	}
+	// 传入数组首地址，第一个元素被更改
+	Fun(array,length);
+	cout << "new data:" << endl;
+	for (int i = 0;i< length;i++)
+	{
+		cout << "Index:" << i << "  " <<"Value:" << array[i] <<endl;
+	}
+	// 单个变量
+	int b = 2;
+	cout << "old const:" << b << endl;	
+	Fun1(b);
+	cout << "new const:" << b << endl;
+}
