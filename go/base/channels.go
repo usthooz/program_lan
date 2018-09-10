019ad8e95f2e525eaa8e602920bfc5cdd21d6da4
解闯 <chuang.xie@enai.im>
2018-09-10 15:22:51 +0800

    add comment

--- go/base/channels.go
+++ go/base/channels.go
@@ -7,6 +7,10 @@ import (
 )
 
 var (
+	/*
+		无缓存: len=1
+		有缓存: len=缓存长度
+	*/
 	ch = make(chan int64, 5)
 )
 
@@ -17,8 +21,8 @@ func main() {
 	go func() {
 		for {
 			ts := time.Now().Unix()
-			// 如果无缓冲通道则等待接收方接收完成才继续写入，否则阻塞在此处
-			// 有缓冲通道则持续写入至缓冲已满，等到接收方读取后有空位，则继续写入
+			// 如果无缓存通道则等待接收方接收完成才继续写入，否则阻塞在此处
+			// 有缓存通道则持续写入至缓冲已满，等到接收方读取后有空位，则继续写入
 			ch <- ts
 			fmt.Println("send:", ts)
 			time.Sleep(time.Duration(1) * time.Second)
