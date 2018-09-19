df0982124040e6fb20b7f9030d2271855cb66897
swxctx <swxctxone@yeah.net>
2018-09-19 18:21:04 +0800

    comment fix

--- lua/callback.lua
+++ lua/callback.lua
@@ -1,9 +1,9 @@
--- 定义回调函数，为多函数提供服务
+-- 定义回调函数，为多函数提供服务，只需要完成逻辑，不关心别人怎么来调用我
 function call_back(value)
 	print(value)
 end
 
--- 业务函数，参数为回调函数指针，在内部通过参数执行回调函数的来哟及
+-- 业务函数，参数为回调函数指针，在内部通过参数执行回调函数的逻辑
 function output(key,call_back_func)
     call_back_func(key)
 end
