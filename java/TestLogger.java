108a08e3aaea44b4ae53c19ea8a096e04850a039
swxctx <swxctxone@yeah.net>
2018-11-13 19:09:29 +0800

    Java loging及颜色输出测试

---
+++ java/TestLogger.java
@@ -0,0 +1,18 @@
+import java.util.logging.*;
+import java.io.IOException;
+
+public class TestLogger { 
+    public static void main(String[] args) throws IOException { 
+        Logger log = Logger.getLogger("lavasoft"); 
+        log.setLevel(Level.INFO); 
+        log.info("Info"); 
+        log.warning("Warn");
+
+        System.out.println("\033[36;4m" + "蓝绿-Tracef" + "\033[0m"); 
+        System.out.println("\033[33;4m" + "黄色-Debugf" + "\033[0m"); 
+        System.out.println("\033[32;4m" + "绿色-Infof" + "\033[0m"); 
+        System.out.println("\033[91;4m" + "淡橙色-Warnf" + "\033[0m"); 
+        System.out.println("\033[31;4m" + "红色-Errorf" + "\033[0m"); 
+        System.out.println("\033[35;4m" + "我是什么色-Faltf" + "\033[0m"); 
+    } 
+}
