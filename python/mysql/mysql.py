d2d8e9487fc0af6713c8f0f70f5cebdca38305d6
swxctx <swxctxone@yeah.net>
2019-01-17 10:09:27 +0800

    py-mysql

--- python/mysql/mysql.py
+++ python/mysql/mysql.py
@@ -4,9 +4,9 @@
 import MySQLdb
 
 # 打开数据库连接
-db = MySQLdb.connect("localhost", "root", "0707", "TESTDB", charset='utf8' )
+db = MySQLdb.connect("localhost", "root", "", "test", charset='utf8')
 
-# 使用cursor()方法获取操作游标 
+# 使用cursor()方法获取操作游标
 cursor = db.cursor()
 
 # 使用execute方法执行SQL语句
