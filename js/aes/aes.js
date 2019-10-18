ae08f3e968e6f6e4f1d5f1a3a2a7a539a29e7f08
swxctx <swxctxone@yeah.net>
2019-10-18 22:19:26 +0800

    add js aes

--- js/aes/aes.js
+++ js/aes/aes.js
@@ -1,24 +1,24 @@
-var crypto = require('crypto');
+// var crypto = require('crypto');
 
-// 秘钥
-var key = "5FXBBDDC6DVPCU84OE7FUMDT6SB47QRF"
-// 填充(ZeroPadding)
-var iv = ''
+// // 秘钥
+// var key = "5FXBBDDC6DVPCU84OE7FUMDT6SB47QRF"
+// // 填充(ZeroPadding)
+// var iv = ''
 
-// init cipher
-const cipher = crypto.createCipheriv('aes-256-ecb', key, iv);
-// 加密
-var cipherResult = cipher.update("usthooz", 'utf8', 'hex');
-// hex编码
-cipherResult += cipher.final('hex');
-console.log(cipherResult)
+// // init cipher
+// const cipher = crypto.createCipheriv('aes-256-ecb', key, iv);
+// // 加密
+// var cipherResult = cipher.update("usthooz", 'utf8', 'hex');
+// // hex编码
+// cipherResult += cipher.final('hex');
+// console.log(cipherResult)
 
-// init cipher
-var decipher = crypto.createDecipheriv('aes-256-ecb', key, iv);
-// 解密
-var decrypted = decipher.update(cipherResult, 'hex', 'utf8');
-decrypted += decipher.final('utf8');
-console.log(decrypted);
+// // init cipher
+// var decipher = crypto.createDecipheriv('aes-256-ecb', key, iv);
+// // 解密
+// var decrypted = decipher.update(cipherResult, 'hex', 'utf8');
+// decrypted += decipher.final('utf8');
+// console.log(decrypted);
 
 var CryptoJS = require("crypto-js");
 var key = CryptoJS.enc.Utf8.parse("5FXBBDDC6DVPCU84OE7FUMDT6SB47QRF");
